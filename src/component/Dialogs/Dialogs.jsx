64a09ee76e472815a5659faa9b5711917fd14e6b
kilokid <arlomov007@gmail.com>
2023-02-19 23:57:32 +0300

    feat: dialogspage styles & content

--- src/component/Dialogs/Dialogs.jsx
+++ src/component/Dialogs/Dialogs.jsx
@@ -2,8 +2,44 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <ul className={s.dialogs_items}>
+                <li className={s.dialog}>
+                    <span></span>
+                    <div>
+                        <p className={s.name}>Artem</p>
+                        <p className={s.message}>Hi bro</p>
+                    </div>
+                </li>
+                <li className={s.dialog}>
+                    <span></span>
+                    <div>
+                        <p className={s.name}>Vanya</p>
+                        <p className={s.message}>Wat's up</p>
+                    </div>
+                </li>
+                <li className={s.dialog}>
+                    <span></span>
+                    <div>
+                        <p className={s.name}>Egor</p>
+                        <p className={s.message}>How a u?</p>
+                    </div>
+                </li>
+                <li className={s.dialog}>
+                    <span></span>
+                    <div>
+                        <p className={s.name}>Nastya</p>
+                        <p className={s.message}>I love you</p>
+                    </div>
+                </li>
+                <li className={s.dialog}>
+                    <span></span>
+                    <div>
+                        <p className={s.name}>Danya</p>
+                        <p className={s.message}>Fuck you</p>
+                    </div>
+                </li>
+            </ul>
         </div>
     );
 }
