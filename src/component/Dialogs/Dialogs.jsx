cff99b15712bb43da7f5c008aa25596ea715a44f
kilokid <arlomov007@gmail.com>
2023-02-20 23:45:51 +0300

    fix: create dialogs array

--- src/component/Dialogs/Dialogs.jsx
+++ src/component/Dialogs/Dialogs.jsx
@@ -3,14 +3,45 @@ import Dialog from './Dialog/Dialog';
 import s from './Dialogs.module.css';
 
 const Dialogs = () => {
+    const dialogsData = [
+        {
+            id: "0",
+            name: 'Artem',
+            message: 'Hi bro'
+        },
+        {
+            id: '1',
+            name: 'Vanya',
+            message: 'What`s up?'
+        },
+        {
+            id: '2',
+            name: 'Egor',
+            message: 'How a u?'
+        },
+        {
+            id: '3',
+            name: 'Nastya',
+            message: 'I love u'
+        },
+        {
+            id: '4',
+            name: 'Danya',
+            message: 'Fuck u man'
+        },
+        {
+            id: '5',
+            name: 'Tanya',
+            message: 'I miss u'
+        }
+    ];
+
     return (
         <div className={s.dialogs}>
             <ul className={s.dialogs_items}>
-                <Dialog name="Artem" message="Hi bro" id="0" />
-                <Dialog name="Vanya" message="Wat's up" id="1" />
-                <Dialog name="Egor" message="How a u?" id="2" />
-                <Dialog name="Nastya" message="I love you" id="3" />
-                <Dialog name="Danya" message="Fuck  man" id="4" />
+                {dialogsData.map( (dialog) => {
+                    return <Dialog key={dialog.id} name={dialog.name} message={dialog.message} id={dialog.id} />
+                } )}
             </ul>
         </div>
     );
