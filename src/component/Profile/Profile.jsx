512099d923ffed43cdf67f9d980ee4d08c7a21ff
kilokid <arlomov007@gmail.com>
2023-02-20 23:33:59 +0300

    feat: profile-info component

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,25 +1,10 @@
+import ProfileInfo from './ProfileInfo/ProfileInfo.jsx';
 import MyPosts from './MyPosts/MyPosts.jsx';
-
-import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-       <div className={s.content}>
-        <div className={s.hero_block}>
-          <img src='https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/maps-satellite-images/satellite-image-of-globe.jpg' />
-        </div>
-        <div className={s.main_info}>
-          <img src="https://static.prinseps.com/media/uploads/cryptopunk6278.png" />
-          <div>
-            <h3>Your Name</h3>
-            <ul>
-              <li>Date of Birth: 1 january</li>
-              <li>City: Kyiv</li>
-              <li>Education: University of Cambridge</li>
-              <li>Web Site: <a href="https://apple.com">apple.com</a></li>
-            </ul>
-          </div>
-        </div>
+      <div>
+        <ProfileInfo />
         <MyPosts />
       </div>
     );
---
+++ src/component/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,25 @@
+import s from './ProfileInfo.module.css';
+
+const ProfileInfo = () => {
+    return (
+       <div className={s.content}>
+        <div className={s.hero_block}>
+          <img src='https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/maps-satellite-images/satellite-image-of-globe.jpg' />
+        </div>
+        <div className={s.main_info}>
+          <img src="https://static.prinseps.com/media/uploads/cryptopunk6278.png" />
+          <div>
+            <h3>Your Name</h3>
+            <ul>
+              <li>Date of Birth: 1 january</li>
+              <li>City: Kyiv</li>
+              <li>Education: University of Cambridge</li>
+              <li>Web Site: <a href="https://apple.com">apple.com</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default ProfileInfo;
