2292ebaa2421476e979211166a00d4819f22a030
kilokid <arlomov007@gmail.com>
2023-02-21 23:37:07 +0300

    fix: change states

--- src/index.js
+++ src/index.js
@@ -2,66 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import state from './redux/state';
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const postsData = [
-    {
-        id: '0',
-        text: 'why nobody love me?',
-        likes: '14'
-    },
-    {
-        id: '1',
-        text: 'Fuck all.',
-        likes: '234'
-    },
-    {
-        id: '2',
-        text: 'Where a u??',
-        likes: '2'
-    }
-];
-
-const dialogsData = [
-    {
-        id: "0",
-        name: 'Artem',
-        message: 'Hi bro'
-    },
-    {
-        id: '1',
-        name: 'Vanya',
-        message: 'What`s up?'
-    },
-    {
-        id: '2',
-        name: 'Egor',
-        message: 'How a u?'
-    },
-    {
-        id: '3',
-        name: 'Nastya',
-        message: 'I love u'
-    },
-    {
-        id: '4',
-        name: 'Danya',
-        message: 'Fuck u man'
-    },
-    {
-        id: '5',
-        name: 'Tanya',
-        message: 'I miss u'
-    }
-];
-
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App posts={postsData} messages={dialogsData} />
+      <App posts={state.postsData} messages={state.dialogsData} />
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/redux/state.js
@@ -0,0 +1,53 @@
+const state = {
+    dialogsData: [
+        {
+            id: "0",
+            name: 'Artem',
+            message: 'Hi bro'
+        },
+        {
+            id: '1',
+            name: 'Vanya',
+            message: 'What`s up?'
+        },
+        {
+            id: '2',
+            name: 'Egor',
+            message: 'How a u?'
+        },
+        {
+            id: '3',
+            name: 'Nastya',
+            message: 'I love u'
+        },
+        {
+            id: '4',
+            name: 'Danya',
+            message: 'Fuck u man'
+        },
+        {
+            id: '5',
+            name: 'Tanya',
+            message: 'I miss u'
+        }
+    ],
+    postsData: [
+        {
+            id: '0',
+            text: 'why nobody love me?',
+            likes: '14'
+        },
+        {
+            id: '1',
+            text: 'Fuck all.',
+            likes: '234'
+        },
+        {
+            id: '2',
+            text: 'Where a u??',
+            likes: '2'
+        }
+    ]
+};
+
+export default state;
