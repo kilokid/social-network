339443282b46259a95ad5fedb338b3dc5e08e8b2
kilokid <arlomov007@gmail.com>
2023-02-23 01:13:01 +0300

    feat: create input ref & onclick listener

--- src/component/Profile/MyPosts/MyPosts.jsx
+++ src/component/Profile/MyPosts/MyPosts.jsx
@@ -1,14 +1,23 @@
+import { createRef } from 'react';
+
 import Post from './Post/Post';
 
 import s from './MyPosts.module.css';
 
 const MyPosts = ({posts}) => {
+    const inputRef = createRef();
+
+    const createPost = () => {
+        const text = inputRef.current.value;
+        alert(text)
+    }
+
     return (
         <>
             <div className={s.posts_form}>
                 <h2>My posts</h2>
-                <input type="text" placeholder='Your news...'></input>
-                <button type='submit'>Send</button>
+                <input ref={inputRef} type="text" placeholder='Your news...'></input>
+                <button onClick={createPost} type='submit'>Send</button>
             </div>
             <ul className={s.posts}>
                 {posts.map(({text, likes, id}) => {
