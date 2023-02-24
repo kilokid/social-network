db4d2adcedad57b628dd996126f53c3b55ab2967
kilokid <arlomov007@gmail.com>
2023-02-25 01:28:55 +0300

    feat: oop store

--- src/redux/state.js
+++ src/redux/state.js
@@ -71,8 +71,6 @@ const state = {
     ]
 };
 
-window.state = state;
-
 export const addPost = () => {
     if ( !state.profilePage.postText ) return;
 
@@ -95,5 +93,102 @@ export const onChangeText = (text) => {
 export const subscribe = (observer) => {
     rerenderEntriesTree = observer;
 }
+
+// const store = {
+//     _state: {
+//         dialogsData: [
+//             {
+//                 id: "0",
+//                 name: 'Artem',
+//                 message: 'Hi bro'
+//             },
+//             {
+//                 id: '1',
+//                 name: 'Vanya',
+//                 message: 'What`s up?'
+//             },
+//             {
+//                 id: '2',
+//                 name: 'Egor',
+//                 message: 'How a u?'
+//             },
+//             {
+//                 id: '3',
+//                 name: 'Nastya',
+//                 message: 'I love u'
+//             },
+//             {
+//                 id: '4',
+//                 name: 'Danya',
+//                 message: 'Fuck u man'
+//             },
+//             {
+//                 id: '5',
+//                 name: 'Tanya',
+//                 message: 'I miss u'
+//             }
+//         ],
+//         profilePage: {
+//             postsData: [
+//                 {
+//                     id: '0',
+//                     text: 'why nobody love me?',
+//                     likes: '14'
+//                 },
+//                 {
+//                     id: '1',
+//                     text: 'Fuck all.',
+//                     likes: '234'
+//                 },
+//                 {
+//                     id: '2',
+//                     text: 'Where a u??',
+//                     likes: '2'
+//                 }
+//             ],
+//             postText: ''
+//         },
+//         friends: [
+//             {
+//                 id: '0',
+//                 name: 'Me'
+//             },
+//             {
+//                 id: '1',
+//                 name: 'You'
+//             },
+//             {
+//                 id: '2',
+//                 name: 'We'
+//             }
+//         ]
+//     },
+//     getState: () => {
+//         console.log(this._state);
+//     },
+//     addPost: () => {
+//         if ( !this._state.profilePage.postText ) return;
+
+//         const newPosts = {
+//             id: '3',
+//             text: state.profilePage.postText,
+//             likes: '0'
+//         }
+
+//         this._state.profilePage.postsData.push(newPosts);
+//         this._state.profilePage.postText = '';
+//         this.rerenderEntriesTree(state);
+//     },
+//     onChangeText: (text) => {
+//         this._state.profilePage.postText = text;
+//         this.rerenderEntriesTree(this._state);
+//     },
+//     subscribe: (observer) => {
+//         this.rerenderEntriesTree = observer;
+//     },
+//     rerenderEntriesTree: () => {
+//         console.log('change state');
+//     }
+// }
 
 export default state;
