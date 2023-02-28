2b79d164ece9c190ce3e31d29586bce3e36709af
kilokid <arlomov007@gmail.com>
2023-02-28 23:32:03 +0300

    fix: rename state > store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import store from './redux/state';
+import store from './redux/store';
 
 import './index.css';
 import App from './App';
