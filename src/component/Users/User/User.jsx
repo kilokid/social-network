4edd8426fb3b78672536d4ac7b1eff248ec64dae
kilokid <arlomov007@gmail.com>
2023-03-09 23:54:14 +0300

    feat: links to profile for users

--- src/component/Users/User/User.jsx
+++ src/component/Users/User/User.jsx
@@ -1,10 +1,13 @@
+import { NavLink } from 'react-router-dom';
 import s from './User.module.css';
 
 const User = ({user, unfollowUser, followUser, setUsers}) => {
     return (
         <li className={s.user}>
             <div className={s.avatar_block}>
-                <span className={s.ava}></span>
+                <NavLink to='/profile' className={s.user_link}>
+                    <span className={s.ava}></span>
+                </NavLink>
                 {user.followed ?
                     <button
                         onClick={() => unfollowUser(user.id)}
@@ -16,12 +19,14 @@ const User = ({user, unfollowUser, followUser, setUsers}) => {
                     >Follow</button>}
             </div>
             <div className={s.user_info}>
-                <h3>{user.name}</h3>
-                <div className={s.location}>
-                    <span>Test Country,</span>
-                    <span>Test city</span>
-                </div>
-                <p className={s.status}>{user.status}</p>
+                <NavLink to='/profile' className={s.user_info_link}>
+                    <h3>{user.name}</h3>
+                    <div className={s.location}>
+                        <span>Test Country,</span>
+                        <span>Test city</span>
+                    </div>
+                    <p className={s.status}>{user.status}</p>
+                </NavLink>
             </div>
         </li>
     )
