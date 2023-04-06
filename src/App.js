f89e3c74b7be1c6d5154d8dc958892bf52dc653a
kilokid <arlomov007@gmail.com>
2023-04-06 23:39:16 +0300

    ♻️ refactor: Add lazy loading and suspense to route components in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { useEffect } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import HeaderContainer from './component/Header/HeaderContainer';
 import Navbar from './component/Navbar/Navbar';
-import ProfileContainer from './component/Profile/ProfileContainer';
-import DialogsContainer from './component/Dialogs/DialogsContainer';
-import UsersContainer from './component/Users/UsersContainer';
-import MusicContainer from './component/Music/MusicContainer';
-import LoginContainer from './component/Login/LoginContainer';
 import Loader from './component/Commons/Loader/Loader';
 
 import './App.css';
+
+const ProfileContainer = lazy(() => import('./component/Profile/ProfileContainer'));
+const DialogsContainer = lazy(() => import('./component/Dialogs/DialogsContainer'));
+const UsersContainer = lazy(() => import('./component/Users/UsersContainer'));
+const MusicContainer = lazy(() => import('./component/Music/MusicContainer'));
+const LoginContainer = lazy(() => import('./component/Login/LoginContainer'));
 
 const App = ({setInitialLoad, initialLoad}) => {
   useEffect(() => {
@@ -28,14 +29,16 @@ const App = ({setInitialLoad, initialLoad}) => {
         <HeaderContainer />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Routes>
-            <Route path="/dialogs" element={ <DialogsContainer /> } />
-            <Route path="/profile" element={ <ProfileContainer /> } />
-            <Route path="/profile/:userId?" element={ <ProfileContainer /> } />
-            <Route path="/users" element={ <UsersContainer /> } />
-            <Route path="/music" element={ <MusicContainer /> } />
-            <Route path="/login" element={ <LoginContainer /> } />
-          </Routes>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/dialogs" element={ <DialogsContainer /> } />
+              <Route path="/profile" element={ <ProfileContainer /> } />
+              <Route path="/profile/:userId?" element={ <ProfileContainer /> } />
+              <Route path="/users" element={ <UsersContainer /> } />
+              <Route path="/music" element={ <MusicContainer /> } />
+              <Route path="/login" element={ <LoginContainer /> } />
+            </Routes>
+          </Suspense>
         </div>
       </div>
   );
