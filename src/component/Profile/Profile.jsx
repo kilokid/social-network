1daba41d90c62b40a0c17b7956bde55733fa4467
kilokid <arlomov007@gmail.com>
2023-04-19 23:16:46 +0300

    feat: type for profile info component

--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
 
-import ProfileInfo from './ProfileInfo/ProfileInfo.jsx';
+import ProfileInfo from './ProfileInfo/ProfileInfo.tsx';
 import MyPosts from './MyPosts/MyPosts.jsx';
 
 const Profile = memo(({profile, posts, onCreatePost, onDeletePost, setProfileStatus, status, isOwner, savePhotos, saveProfileInfo, userId}) => {
--- src/component/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/component/Profile/ProfileInfo/ProfileInfo.tsx
@@ -4,23 +4,34 @@ import Loader from '../../Commons/Loader/Loader';
 import ProfileStatus from '../ProfileStatus/ProfileStatus';
 import ProfileDataForm from '../ProfileDataForm/ProfileDataForm';
 
+import { ProfileType } from '../../../types/types';
+
 import s from './ProfileInfo.module.css';
 
+type Props = {
+  profile: ProfileType,
+  setStatus: () => void,
+  status: string,
+  isOwner: boolean,
+  savePhotos: (arg0:string) => void,
+  saveProfileInfo: (arg0:Object, arg1:number) => void,
+  userId: number
+}
 
-const ProfileInfo = ({profile, setStatus, status, isOwner, savePhotos, saveProfileInfo, userId}) => {
+const ProfileInfo: React.FC<Props> = ({profile, setStatus, status, isOwner, savePhotos, saveProfileInfo, userId}) => {
   const [editMode, setEditMode] = useState(false);
 
   if (!profile) {
     return <Loader />
   }
 
-  const onSaveNewAvatarPhoto = (e) => {
+  const onSaveNewAvatarPhoto = (e: any) => {
     if (e.target.files.length) {
       savePhotos(e.target.files[0]);
     }
   }
 
-  const onSubmit = (formData) => {
+  const onSubmit = (formData: any) => {
     saveProfileInfo(formData, userId);
     setEditMode(false);
   }
@@ -34,7 +45,7 @@ const ProfileInfo = ({profile, setStatus, status, isOwner, savePhotos, saveProfi
       </div>
       <div className={s.main_info}>
         <img src={profile.photos.large ? profile.photos.large : withOutAvatar} alt={profile.fullName} />
-        {isOwner && <input type="file" onChange={onSaveNewAvatarPhoto} />}
+        {/* {isOwner && <input type="file" onChange={onSaveNewAvatarPhoto} />} */}
         <div>
           {editMode ? <ProfileDataForm profile={profile} onSubmit={onSubmit} /> : <ProfileData editProfileInfo={() => setEditMode(true)} profile={profile} setStatus={setStatus} status={status} />}
         </div>
@@ -50,7 +61,7 @@ const ProfileData = ({profile, setStatus, status, editProfileInfo}) => {
         <ul>
           <ProfileStatus updateStoreStatus={setStatus} storeStatus={status} />
           <li>About me: {profile.aboutMe}</li>
-          <li>Looking for a job: {profile.lookingForAJob ? 'yes' : 'no'}</li>
+          { profile.lookingForAJob && <li>Looking for a job</li>}
           {profile.lookingForAJob && <li>Job Description: {profile.lookingForAJobDescription}</li>}
           {Object.keys(profile.contacts).map(key => {
             return <li key={key}>{key}: {profile.contacts[key]}</li>
