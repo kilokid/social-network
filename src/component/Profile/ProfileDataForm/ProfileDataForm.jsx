d05052c6f06dd8beb106dcbdd1bcd7f41c97aca8
kilokid <arlomov007@gmail.com>
2023-04-28 00:24:38 +0300

    ✨ feat: Add Music and News components CSS and update ProfileDataForm
    This commit adds the CSS files for the Music and News components, which include a notice style. Additionally, the ProfileDataForm component has been updated to include styling and labels for the input fields, and a new title. The changes aim to improve the visual appearance and user experience of the application.

--- src/component/Profile/ProfileDataForm/ProfileDataForm.jsx
+++ src/component/Profile/ProfileDataForm/ProfileDataForm.jsx
@@ -1,51 +1,61 @@
 import { useForm } from "react-hook-form";
 
+import s from './ProfileDataForm.module.css';
+
 const ProfileDataForm = ({profile, onSubmit}) => {
     const {register, handleSubmit, formState: {errors}} = useForm();
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <h3>
-                <input
-                    {...register('fullName')}
-                    type='text'
-                    placeholder='Full name'
-                />
-            </h3>
-            <ul>
-                <li>
+        <>
+        <h3 className={s.title}>Edit profile info</h3>
+        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+            <ul className={s.items_wrapper}>
+                <li className={s.item}>
+                    Full name:
+                    <input
+                    className={s.field}
+                        {...register('fullName')}
+                        type='text'
+                        placeholder='Full name'
+                    />
+                </li>
+                <li className={s.item}>About me:
                     <textarea
+                    className={s.field}
                         {...register('aboutMe')}
                         type='text'
                         placeholder='About me'
                     />
                 </li>
-                <li>Looking for a job:
+                <li className={s.item}>Looking for a job:
                     <input
                         {...register('lookingForAJob')}
                         type='checkbox'
                     />
                 </li>
-                <li>
+                <li className={s.item}>My professional skills:
                      <textarea
+                     className={s.field}
                         {...register('lookingForAJobDescription')}
                         type='text'
                         placeholder='My professional skills'
                     />
                 </li>
                 {Object.keys(profile.contacts).map(key => {
-                    return <li key={key}>{key}:
+                    return <li className={s.item} key={key}>{key}:
                     <input
+                    className={s.field}
                         {...register(`contacts.${key}`)}
                         type='text'
                         placeholder={key}
                     />
-                    {profile.contacts[key]}
+                    {/* {profile.contacts[key]} */}
                     </li>
                 })}
             </ul>
-            <button type='submit'>Save</button>
+            <button className={s.save} type='submit'>Save</button>
         </form>
+        </>
     )
 }
 
